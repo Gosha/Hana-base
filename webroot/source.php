0f1a9cdb5ba24f9f938d600390e44fa06219dd2c
Georgij Zacharov <gosha.za@gmail.com>
2014-09-16 19:51:56 +0200

    Add source.php

---
+++ webroot/source.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * This is a Hana pagecontroller.
+ *
+ */
+// Include the essential config-file which also creates the $hana variable with its defaults.
+include(__DIR__.'/config.php');
+
+$hana['title'] = "Källkod";
+
+// Add style for csource
+$hana['stylesheets'][] = 'css/source.css';
+
+// Create the object to display sourcecode
+//$source = new CSource();
+$source = new CSource(array('secure_dir' => '..', 'base_dir' => '..'));
+
+$hana['main'] = <<<EOD
+<h1>Källkod</h1>
+{$source->View()}
+EOD;
+
+$hana['footer'] = <<<EOD
+<footer><span class='sitefooter'><a href='https://github.com/GoshaZa/Hana-base'>Hana på GitHub</a></span></footer>
+EOD;
+
+
+// Finally, leave it all to the rendering phase of Hana.
+include(HANA_THEME_PATH);
