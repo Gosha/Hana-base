61c055dbec047e7dcc218e28bed7d7e6fbfda2cc
Georgij Zacharov <gosha.za@gmail.com>
2014-10-13 00:00:36 +0200

    Report text kmom02

--- webroot/reports.php
+++ webroot/reports.php
@@ -65,7 +65,62 @@ $hana['main'] = <<<EOD
     egna <code>webroot/source.php</code>. Inga konstigheter.
   </p>
 </div>
+<div class="grid-50">
+  <h2>Kmom02</h2>
+  <h3>Tidigare erfarenheter</h3>
 
+  <p>
+    Började med objektorienterad programmering för så länge sedan att
+    jag inte riktigt minns när det var. Hade under lång tid ingen koll
+    på när och hur det skulle användas. Men efter några skolprojekt på
+    universitetet såg hur det kan vara användbart. På universitetet
+    var det Java som gällde men har även lite grundläggande kunskap om
+    bland annat PHP, Ruby och C++.
+  </p>
+  <p>
+    Sedan Java-kursen har jag blivit ganska irriterad över hur t.ex.
+    Wordpress blandar proceduell och objektorienterad programmering.
+    Det gör koden man själv skriver väldigt rörig.
+  </p>
+  <p>
+    När jag tidigare skrivit PHP har jag använt mig av
+    objektorienterade bibliotek, men har inte skrivit någon egen
+    objektorienterad kod.
+  </p>
+  <h3>Guiden</h3>
+  <p>
+    Måste vara ärlig och säga att jag skumläste mig genom
+    oophp20-guiden. Skumläste även kurslitteraturen. Det mesta är
+    väldigt likt andra objektorienterade språk.
+  </p>
+  <p>
+    Saknade Rubys mixins, men såg i guiden att PHP erbjuder traits
+    vilket verkar vara precis det jag letade efter. I slutändan använde
+    jag mig dock inte av dem i uppgiften.
+  </p>
+  <h3>Uppgiften</h3>
+  <p>
+    Valde att implementera tärningsspelet 100.
+  </p>
+  <p>
+    Valde att skippa rekommendationerna om vilka klasser som skulle
+    implementeras. En klass för tärningen, <code>CDice</code>, och för
+    spelet, <code>CDiceGame</code> hängde dock med.
+  </p>
+  <p>
+    För att abstrahera sparandet av variabler i <code>\$_SESSION</code>
+    införde jag <code>CSessionVar</code> som vid instantiering läser
+    från <code>\$_SESSION</code> ifall variabeln redan existerar och
+    annars skapar en ny. Vid destruktion sparar den variabeln i
+    <code>\$_SESSION</code>.
+  </p>
+  <p>
+    För att få till lite klass-arv implementerade
+    jag <code>CDiceSession</code> som subklass
+    till <code>CSessionVar</code>. <code>CSessionVar</code>
+    abstraherar en runda av tärnkastningar.
+  </p>
+</div>
 EOD;
 
 
