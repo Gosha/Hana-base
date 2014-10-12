d0feee0e94cb1cefddf48210efe0ff538ec58526
Georgij Zacharov <gosha.za@gmail.com>
2014-10-12 17:52:06 +0200

    Forgot to commit CDiceGame

---
+++ src/CDiceGame/CDiceGame.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * Class for a dice session
+ *
+ * Encapsulates the saving and restoring of dice
+ */
+class CDiceGame
+{
+  const STATE = 'gamestate';
+  private $state;
+  private $dice;
+  private $method;
+
+  public function __construct($method = "GET") {
+    $this->method = $method;
+    $this->state = new CSessionVar($this::STATE,
+                                   array(
+                                         'round' => 0,
+                                         'score' => 0,
+                                         ));
+    $this->dice = new CDiceSession();
+    $this->handleVars();
+  }
+
+  private function handleVars() {
+    if ($this->method == "GET") {
+      $vars = &$_GET;
+    } else {
+      $vars = &$_POST;
+    }
+
+    if(!isset($vars['game_action']))
+      return;
+
+    switch($vars['game_action']) {
+    case 'roll':
+      $this->roll();
+      break;
+    case 'save':
+      $this->save();
+      break;
+    case 'clear':
+      $this->clear();
+      break;
+    }
+  }
+
+  private function roll() {
+    $this->dice->addDice(new CDice());
+  }
+
+  private function save() {
+    $this->state->get()['score'] += $this->dice->sum();
+    $this->state->get()['round'] += 1;
+    $this->dice->clearDice();
+  }
+
+  private function clear() {
+    $this->dice->clearDice();
+    $this->state->clear();
+  }
+
+  public function printGame() {
+    $ret = "";
+    $curURL = current_url();
+
+    $ret .= <<<EOF
+      <br>
+      <a href="{$curURL}?game_action=roll">Kasta</a>
+      <a href="{$curURL}?game_action=save">Spara</a>
+      <a href="{$curURL}?game_action=clear">Rensa</a>
+      <br><br>
+EOF;
+
+    foreach($this->dice->getDice() as $die) {
+      $ret .= $die->getImage() ."\n ";
+    }
+
+    $ret .= dump($this->state->get(), 1);
+
+    return $ret;
+  }
+}
